41e31837d806be69adfb2aaa30893214f59af712
Kevin Worcester <kworcester@misfitcode.com>
2007-08-15 03:35:20 +0000

    Added auto-free pointers.

---
+++ src/libmm3d/local_ptr.h
@@ -0,0 +1,35 @@
+#ifndef __LOCAL_PTR_H
+#define __LOCAL_PTR_H
+
+#include <stdlib.h>
+
+template<typename T> class local_ptr
+{
+   public:
+      local_ptr( T * pval = NULL )
+         : m_pval( pval ) {}
+      virtual ~local_ptr() { free_ptr(); }
+
+      T * get() { return m_pval; }
+      T * reset(T * newPval) { free_ptr(); return m_pval = newPval; }
+      const T * get() const { return m_pval; }
+      const T * reset(T * newPval) const { free_ptr(); return m_pval = newPval; }
+
+      T & operator*() { return *m_pval; }
+      T * operator->() { return m_pval; }
+      const T & operator*() const { return *m_pval; }
+      const T * operator->() const { return m_pval; }
+
+      T * operator=(T* newPval) { return reset(newPval); }
+      const T * operator=(T* newPval) const { return reset(newPval); }
+
+      bool operator!() const { return m_pval == NULL; }
+      bool isnull() const { return m_pval == NULL; }
+
+   protected:
+      void free_ptr() const { delete m_pval; m_pval = NULL; }
+
+      mutable T * m_pval;
+};
+
+#endif // __LOCAL_PTR_H
