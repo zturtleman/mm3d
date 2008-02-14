ab6778aaa0be7e107c844d486f9e1040cb24f96b
Kevin Worcester <kworcester@misfitcode.com>
2008-02-14 05:25:15 +0000

    Added translation, undomgr, and joint tests. Added filter tests for mm3d, obj, and md2.

---
+++ src/tests/libmm3d/filter_md2_test.cc
@@ -0,0 +1,188 @@
+/*  Misfit Model 3D
+ * 
+ *  Copyright (c) 2007-2008 Kevin Worcester
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, 
+ *  USA.
+ *
+ *  See the COPYING file for full license text.
+ */
+
+
+// This file tests the MD2 model file filter.
+
+// FIXME fix tolerance issue
+
+#include <QtTest/QtTest>
+#include <unistd.h>
+
+#include "test_common.h"
+#include "testfilefactory.h"
+
+#include "model.h"
+#include "texture.h"
+#include "modelstatus.h"
+#include "log.h"
+#include "md2filter.h"
+#include "mm3dfilter.h"
+
+#include "local_array.h"
+#include "local_ptr.h"
+#include "release_ptr.h"
+
+#include "texmgr.h"
+#include "pcxtex.h"
+#include "tgatex.h"
+#include "rawtex.h"
+#include "qttex.h"
+
+int init_std_texture_filters()
+{
+   log_debug( "initializing standard texture filters\n" );
+
+   TextureManager * textureManager = TextureManager::getInstance();
+
+   TextureFilter * filter;
+
+   filter = new TgaTextureFilter;
+   textureManager->registerTextureFilter( filter );
+   filter = new RawTextureFilter;
+   textureManager->registerTextureFilter( filter );
+   filter = new PcxTextureFilter;
+   textureManager->registerTextureFilter( filter );
+   filter = new QtTextureFilter;
+   textureManager->registerTextureFilter( filter );
+
+   return 0;
+}
+
+
+Model * loadModelOrDie( ModelFilter & filter, const char * filename )
+{
+   Model * model = new Model;
+   model->forceAddOrDelete( true );
+
+   Model::ModelErrorE err = filter.readFile( model, filename );
+
+   if ( err != Model::ERROR_NONE )
+   {
+      fprintf( stderr, "fatal: read %s: %s\n", filename, Model::errorToString( err ) );
+      delete model;
+      exit( -1 );
+   }
+
+   model->loadTextures();
+   model->forceAddOrDelete( true );
+   return model;
+}
+
+Model * loadMm3dOrDie( const char * filename, FileFactory * factory = NULL )
+{
+   MisfitFilter f;
+   if ( factory )
+      f.setFactory( factory );
+   return loadModelOrDie( f, filename );
+}
+
+Model * loadMd2OrDie( const char * filename, FileFactory * factory = NULL )
+{
+   Md2Filter f;
+   if ( factory )
+      f.setFactory( factory );
+   return loadModelOrDie( f, filename );
+}
+
+void saveMd2OrDie( Model * model, const char * filename, FileFactory * factory = NULL )
+{
+   Md2Filter filter;
+   if ( factory )
+      filter.setFactory( factory );
+
+   Model::ModelErrorE err = filter.writeFile( model, filename );
+
+   if ( err != Model::ERROR_NONE )
+   {
+      fprintf( stderr, "fatal: write %s: %s\n", filename, Model::errorToString( err ) );
+      delete model;
+      exit( -1 );
+   }
+}
+
+void model_status( Model * model, StatusTypeE type, unsigned ms, const char * fmt, ... )
+{
+   // FIXME hack
+}
+
+
+class FilterMd2Test : public QObject
+{
+   Q_OBJECT
+private:
+   void testModelFile( const char * lhs_file, const Model * rhs )
+   {
+      // The lhs pointer is from the original filter
+      local_ptr<Model> lhs = loadMm3dOrDie( lhs_file );
+
+      int bits = ~Model::CompareMeta;
+      QVERIFY_EQ( bits, lhs->equal( rhs, bits, 0.008 ) );
+   }
+
+   void testReadAndWrite( const char * infile, const char * outfile,
+         const char * reffile )
+   {
+      TestFileFactory factory;
+      local_ptr<Model> m = loadMd2OrDie( infile, &factory );
+      testModelFile( reffile, m.get() );
+
+      saveMd2OrDie( m.get(), outfile, &factory );
+      m = loadMd2OrDie( outfile, &factory );
+      testModelFile( reffile, m.get() );
+   }
+
+private slots:
+
+   void initTestCase()
+   {
+      init_std_texture_filters();
+      log_enable_debug( false );
+      log_enable_warning( false );
+      log_enable_error( false );
+   }
+
+   void testMd2ModelA()
+   {
+      testReadAndWrite(
+            "filtertest/md2/hellpig/hellpig.md2",
+            "filtertest/md2/hellpig/hellpig.md2",
+            "filtertest/md2/hellpig/hellpig.mm3d" );
+   }
+
+   void testMd2ModelB()
+   {
+      testReadAndWrite(
+            "filtertest/md2/tekkblade/tekkblade.md2",
+            "filtertest/md2/tekkblade/tekkblade.md2",
+            "filtertest/md2/tekkblade/tekkblade.mm3d" );
+   }
+
+   // FIXME add tests:
+   //   read
+   //   write
+   //   error handling
+};
+
+QTEST_MAIN(FilterMd2Test)
+#include "filter_md2_test.moc"
+
---
+++ src/tests/libmm3d/translation_test.cc
@@ -0,0 +1,79 @@
+/*  Misfit Model 3D
+ * 
+ *  Copyright (c) 2007-2008 Kevin Worcester
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, 
+ *  USA.
+ *
+ *  See the COPYING file for full license text.
+ */
+
+
+// This file tests translate.cc and mlocale.cc
+
+#include <QtTest/QtTest>
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "test_common.h"
+
+#include "translate.h"
+#include "mlocale.h"
+
+std::string test_translate_function( const char * sourceStr )
+{
+   if ( strcmp( sourceStr, "foo" ) == 0 )
+      return "goo";
+   else if ( strcmp( sourceStr, "bar" ) == 0 )
+      return "ber";
+   return sourceStr;
+}
+
+class TranslationTest : public QObject
+{
+   Q_OBJECT
+private slots:
+
+   void testNoTranslate()
+   {
+      transll_install_handler( NULL );
+      QVERIFY_EQ( std::string("foo"), transll(QT_TRANSLATE_NOOP("Test", "foo")) );
+      QVERIFY_EQ( std::string("bar"), transll(QT_TRANSLATE_NOOP("Test", "bar")) );
+      QVERIFY_EQ( std::string("baz"), transll(QT_TRANSLATE_NOOP("Test", "baz")) );
+   }
+
+   void testTranslate()
+   {
+      transll_install_handler( test_translate_function );
+      QVERIFY_EQ( std::string("goo"), transll(QT_TRANSLATE_NOOP("Test", "foo")) );
+      QVERIFY_EQ( std::string("ber"), transll(QT_TRANSLATE_NOOP("Test", "bar")) );
+      QVERIFY_EQ( std::string("baz"), transll(QT_TRANSLATE_NOOP("Test", "baz")) );
+      transll_install_handler( NULL );
+   }
+
+   void testLocale()
+   {
+      QVERIFY_EQ( std::string(""), mlocale_get() );
+      mlocale_set("en-GB");
+      QVERIFY_EQ( std::string("en-GB"), mlocale_get() );
+      mlocale_set("");
+      QVERIFY_EQ( std::string(""), mlocale_get() );
+   }
+};
+
+QTEST_MAIN(TranslationTest)
+#include "translation_test.moc"
